c7d914bf15bb9f6c180f52fda2dedc97d34bce22
Macovei Tudor <tudor_macovei@iCloud.com>
2024-01-21 16:18:57 +0100

    Change draw logic from operating on an image to render

--- main.cpp
+++ main.cpp
@@ -8,13 +8,13 @@ const int SCREEN_HEIGHT = 480;
 // SDL requires specifically this signature for main
 int main(int argc, char* args[])
 {
-	//The window we'll be rendering to
+	// The window we'll be rendering to
 	SDL_Window* window = NULL;
 
-	//The surface contained by the window
+	// The surface contained by the window
 	SDL_Surface* screenSurface = NULL;
 
-	//Initialize SDL
+	// Initialize SDL
 	if (SDL_Init(SDL_INIT_VIDEO) < 0)
 	{
 		printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
@@ -29,14 +29,16 @@ int main(int argc, char* args[])
 		}
 		else
 		{
-			// Get window surface
-			screenSurface = SDL_GetWindowSurface(window);
+			SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
+			
+			// We render with a color of choice at a time		
+			SDL_SetRenderDrawColor(renderer, 0xFF, 0xFF, 0xFF, SDL_ALPHA_OPAQUE);
 
 			// Fill the surface white
-			SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0xAF, 0xBF, 0x9F));
-
-			// Update the surface, otherwise what has been drawn will not be seen
-			SDL_UpdateWindowSurface(window);
+			SDL_RenderClear(renderer);
+			
+			// Present the render, otherwise what has been drawn will not be seen
+			SDL_RenderPresent(renderer);
 
 			// Hack to get window to stay up
 			SDL_Event e; bool quit = false; while (quit == false) { while (SDL_PollEvent(&e)) { if (e.type == SDL_QUIT) quit = true; } }
