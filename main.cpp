13ce96287af79a64673ab599bb631ad284cefa31
Macovei Tudor <tudor_macovei@iCloud.com>
2024-01-22 11:00:05 +0100

    Add heightmap output for 64x64 images

--- main.cpp
+++ main.cpp
@@ -1,15 +1,16 @@
 #include <SDL.h>
 #include <stdio.h>
+#include <iostream>
 #include <vector>
+#include "lodepng.h"
+#include "Eigen/Core"
 
 // Screen dimension constants
-const int SCREEN_WIDTH = 640;
-const int SCREEN_HEIGHT = 480;
+const int SCREEN_WIDTH = 950;
+const int SCREEN_HEIGHT = 600;
 
-const int HEIGHTMAP_ROWS = 4;
-const int HEIGHTMAP_COLS = 4;
-
-const float heightmap[HEIGHTMAP_ROWS][HEIGHTMAP_COLS]{ {0, 0, 0, 0}, {0, 0, 0, 0}, {0, 0, 0, 0}, {0, 0, 0, 0}};
+int HEIGHTMAP_ROWS = 4;
+int HEIGHTMAP_COLS = 4;
 
 typedef struct s_point {
 	float x, y, z;
@@ -51,32 +52,45 @@ void lines_scale(std::vector<t_line> &lines, float factor) {
 	fprintf(stdout, "exiting lines_scale\n");
 }
 
-void draw_heightmap(SDL_Renderer *renderer) {
-	// We render with a color of choice at a time		
-	SDL_SetRenderDrawColor(renderer, 0xFF, 0xFF, 0xFF, SDL_ALPHA_OPAQUE);
-	
-	std::vector<t_line> lines;
-	for (int i = 0; i < HEIGHTMAP_ROWS; i++)
+void lines_from_heightmap(int** heightmap, int width, int height, std::vector<t_line> &lines) {
+	for (int i = 0; i < height; i++)
 	{
-		for (int j = 0; j < HEIGHTMAP_COLS; j++)
+		for (int j = 0; j < width; j++)
 		{
-			t_point current = { i, j, heightmap[i][j]};
+			float z_fact = -10;
+			float z_trans = 16.1f;
+			t_point current = { i, j, z_fact * heightmap[i][j] / 255.0f + z_trans };
 			if (i > 0)
 			{
-				t_point up = { i - 1, j, heightmap[i - 1][j] };
+				t_point up = { i - 1, j, z_fact * heightmap[i - 1][j] / 255.0f + z_trans };
 				lines.push_back(t_line(current, up));
 			}
 			if (j > 0)
 			{
-				t_point left = { i, j - 1, heightmap[i][j - 1] };
+				t_point left = { i, j - 1, z_fact * heightmap[i][j - 1] / 255.0f + z_trans };
 				lines.push_back(t_line(current, left));
 			}
-				
+
 		}
 	}
+}
+
+void draw_heightmap(SDL_Renderer *renderer, int **heightmap, int width, int height) {
+	// We render with a color of choice at a time		
+	
+	SDL_SetRenderDrawColor(renderer, 0xFF, 0x00, 0x00, 0xFF); // red background
+	SDL_RenderClear(renderer);
+	SDL_SetRenderDrawBlendMode(renderer, SDL_BlendMode::SDL_BLENDMODE_ADD);
+
+	SDL_SetRenderDrawColor(renderer, 0x0F, 0xFF, 0xFF, 0x4F);
+	
+	std::vector<t_line> lines;
+	lines_from_heightmap(heightmap, width, height, lines);
+	
 	// transform lines so we can visualize them
-	lines_scale(lines, 100);
+	lines_scale(lines, 10);
 	basis_transform(lines, { 1.0f, 0, 0 }, { 0.5f, 0.5f, 0 }, { 0, 1.0f, 0 });
+	
 	for (const t_line &line : lines)
 	{
 		SDL_RenderDrawLine(renderer, line.first.x, line.first.y, line.second.x, line.second.y);
@@ -88,7 +102,35 @@ void draw_heightmap(SDL_Renderer *renderer) {
 // SDL requires specifically this signature for main
 int main(int argc, char* args[])
 {
-	fprintf(stdout, "exiting lines_scale\n");
+	std::vector<unsigned char> image; //the raw pixels
+	unsigned width, height;
+
+	//decode
+	unsigned error = lodepng::decode(image, width, height, "heightmap_64.png");
+
+	//if there's an error, display it
+	if (error) {
+		std::cout << "decoder error " << error << ": " << lodepng_error_text(error) << std::endl;
+		return -1;
+	}
+
+	//the pixels are now in the vector "image", 4 bytes per pixel, ordered RGBARGBA..., use it as texture, draw it, ...
+
+	// Get image dimensions
+
+	// Create a 2D array to store pixel values
+	int** pixelValues = new int* [height];
+	for (int i = 0; i < height; ++i) {
+		pixelValues[i] = new int[width];
+	}
+
+	// Copy pixel values to the array
+	for (int i = 0; i < height; ++i) {
+		for (int j = 0; j < width; ++j) {
+			pixelValues[i][j] = static_cast<int>(image[(i * height + j) * 4]);
+		}
+	}
+	// Now Use the pixelValues array as needed
 
 	// The window we'll be rendering to
 	SDL_Window* window = NULL;
@@ -113,7 +155,7 @@ int main(int argc, char* args[])
 		{
 			SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
 			
-			draw_heightmap(renderer);
+			draw_heightmap(renderer, pixelValues, width, height);
 
 			// Hack to get window to stay up
 			SDL_Event e; bool quit = false; while (quit == false) { while (SDL_PollEvent(&e)) { if (e.type == SDL_QUIT) quit = true; } }
@@ -125,6 +167,13 @@ int main(int argc, char* args[])
 
 	// Quit SDL subsystems
 	SDL_Quit();
+
+
+	// Don't forget to free the allocated memory
+	for (int i = 0; i < height; ++i) {
+		delete[] pixelValues[i];
+	}
+	delete[] pixelValues;
 
 	return 0;
 }
